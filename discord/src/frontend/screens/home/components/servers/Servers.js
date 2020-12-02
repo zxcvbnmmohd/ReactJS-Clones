5d04f8fe79eb4e9dfe17fca938019b16c23f1857
Mohamed Mohamed <studio@mohd.ca>
2020-12-01 20:32:15 -0500

    Added Modal button

--- discord/src/frontend/screens/home/components/servers/Servers.js
+++ discord/src/frontend/screens/home/components/servers/Servers.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
 import AddIcon from "@material-ui/icons/Add";
 // import { getCurrentUser } from "../../../../../backend/redux/reducers/authReducer";
 import {
@@ -14,46 +16,65 @@ function Servers() {
   const dispatch = useDispatch();
   const servers = useSelector(getServers);
   const currentServer = useSelector(getCurrentServer);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className="servers">
       <div className="holder">
-        {
-          currentServer === null ? <div className="line"></div> : <div className="space"></div>
-        }
-        
-        <div className="servers__me" onClick={() => dispatch(setCurrentServer(null))} >
+        {currentServer === null ? (
+          <div className="line"></div>
+        ) : (
+          <div className="space"></div>
+        )}
+
+        <div
+          className="servers__me"
+          onClick={() => dispatch(setCurrentServer(null))}
+        >
           <h5>ME</h5>
         </div>
 
         <div className="space"></div>
       </div>
 
-
-      {
-        servers.map((server) => {
-          return (
-            <div key={server.serverID} className="holder">
-              {
-                currentServer === null
-                  ? <div className="space"></div>
-                  : currentServer.serverID === server.serverID
-                    ? <div className="dot"></div>
-                    : <div className="space"></div>
-              }
-
-              <div className="servers__server" onClick={() => dispatch(setCurrentServer(server))} >
-                <p>{server.serverID.substring(0, 3)}</p>
-              </div>
-
+      {servers.map((server) => {
+        return (
+          <div key={server.serverID} className="holder">
+            {currentServer === null ? (
               <div className="space"></div>
-            </div>
-          );
-        })
-      }
+            ) : currentServer.serverID === server.serverID ? (
+              <div className="dot"></div>
+            ) : (
+              <div className="space"></div>
+            )}
 
-      <div className="servers__add">
+            <div
+              className="servers__server"
+              onClick={() => dispatch(setCurrentServer(server))}
+            >
+              <p>{server.serverID.substring(0, 3)}</p>
+            </div>
+
+            <div className="space"></div>
+          </div>
+        );
+      })}
+
+      <div className="servers__add" onClick={handleOpen}>
         <AddIcon />
+        <Modal open={open} onClose={handleClose}>
+          <div>
+            Hello
+          </div>
+        </Modal>
       </div>
     </div>
   );
